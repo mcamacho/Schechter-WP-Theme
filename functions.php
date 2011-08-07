a200e1c3b4ab3c5c5223fb4f0e3ee295b31e4ad9
Mauricio Camacho <mcamacho@bbmg.com>
2011-08-07 09:36:37 -0400

    functions updated

--- functions.php
+++ functions.php
@@ -1,14 +1,82 @@
 <?php
+/**
+ * 
+ */
 
+add_action( 'after_setup_theme', 'twentyeleven_setup' );
+
+function twentyeleven_setup() {
+	
+	//remove twentyeleven widgets init
+	remove_action( 'widgets_init', 'twentyeleven_widgets_init' );
+	
+	/* Make Twenty Eleven available for translation.
+	 * Translations can be added to the /languages/ directory.
+	 * If you're building a theme based on Twenty Eleven, use a find and replace
+	 * to change 'twentyeleven' to the name of your theme in all the template files.
+	 */
+	load_theme_textdomain( 'twentyeleven', TEMPLATEPATH . '/languages' );
+
+	$locale = get_locale();
+	$locale_file = TEMPLATEPATH . "/languages/$locale.php";
+	if ( is_readable( $locale_file ) )
+		require_once( $locale_file );
+
+	// This theme styles the visual editor with editor-style.css to match the theme style.
+	add_editor_style();
+
+	// Grab Image_Text_WIdget.
 	require( dirname( __FILE__ ) . '/inc/widgets.php' );
 
+	// Add default posts and comments RSS feed links to <head>.
+	add_theme_support( 'automatic-feed-links' );
+
+	// This theme uses wp_nav_menu() sidebar.
+	register_nav_menu( 'primary', __( 'Primary Menu', 'twentyeleven' ) );
+	
 	// Add menu for the footer.
 	register_nav_menu( 'footer', __( 'Foot Menu', 'twentyeleven' ) );
+
+	// Add support for a variety of post formats
+	add_theme_support( 'post-formats', array( 'aside', 'link', 'gallery', 'status', 'quote', 'image' ) );
+
+	// Add support for custom backgrounds
+	add_custom_background();
+
+	// This theme uses Featured Images (also known as post thumbnails) for per-post/per-page Custom Header images
+	add_theme_support( 'post-thumbnails' );
+
+	
+}// twentyeleven_setup
+
+function schechter_widgets_init() {
+	
+	// register FooWidget widget
+	register_widget("Image_Text_Widget");
+	
+	register_sidebar( array(
+		'name' => __( 'Main Sidebar', 'twentyeleven' ),
+		'id' => 'sidebar-1',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
 	
 	//Add a footer sidebar for the home template
 	register_sidebar( array(
 		'name' => __( 'Home Footer', 'twentyeleven' ),
 		'id' => 'sidebar-home',
+		'description' => __( 'widget area for the home area over footer', 'twentyeleven' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );	
+
+	register_sidebar( array(
+		'name' => __( 'Footer Area One', 'twentyeleven' ),
+		'id' => 'sidebar-3',
 		'description' => __( 'An optional widget area for your site footer', 'twentyeleven' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget' => "</aside>",
@@ -16,8 +84,19 @@
 		'after_title' => '</h3>',
 	) );
 
+	register_sidebar( array(
+		'name' => __( 'Footer Area Two', 'twentyeleven' ),
+		'id' => 'sidebar-4',
+		'description' => __( 'An optional widget area for your site footer', 'twentyeleven' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+	
+	
+}
 
-	// register FooWidget widget
-	register_widget("Image_Text_Widget");
+add_action( 'widgets_init', 'schechter_widgets_init' );
 	
 ?>
